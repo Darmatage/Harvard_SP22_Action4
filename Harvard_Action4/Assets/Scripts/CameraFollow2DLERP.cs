c9a7e9590d6b84485b30e54f8f0b7ff79f03bfdc
andy188w <98664542+andy188w@users.noreply.github.com>
2022-04-18 15:53:35 -0400

    Camera Speed Adjusted

--- Harvard_Action4/Assets/Scripts/CameraFollow2DLERP.cs
+++ Harvard_Action4/Assets/Scripts/CameraFollow2DLERP.cs
@@ -6,19 +6,19 @@ public class CameraFollow2DLERP : MonoBehaviour
 {
 
     public GameObject target;
-    public float camSpeed = 4.0f;
+    public float camSpeed = 20.0f;
 
     void Start()
     {
         target = GameObject.FindWithTag("Player");
     }
 
- 
+
 
 
     void FixedUpdate()
     {
-        
+
         Vector2 pos = Vector2.Lerp((Vector2)transform.position, (Vector2)target.transform.position, camSpeed * Time.fixedDeltaTime);
         transform.position = new Vector3(pos.x, pos.y, transform.position.z);
 
