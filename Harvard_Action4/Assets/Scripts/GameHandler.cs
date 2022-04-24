4b93b31eb95e2b09875745ebd4bc857222e18649
AlanApogee <alangvhuang@outlook.com>
2022-04-24 22:15:57 +0800

    SceneEnd update

--- Harvard_Action4/Assets/Scripts/GameHandler.cs
+++ Harvard_Action4/Assets/Scripts/GameHandler.cs
@@ -163,7 +163,7 @@ public class GameHandler : MonoBehaviour
 		player.GetComponent<PlayerMove>().isAlive = false;
 		player.GetComponent<PlayerJump>().isAlive = false;
 		yield return new WaitForSeconds(1.0f);
-		SceneManager.LoadScene("EndLose");
+		SceneManager.LoadScene("SceneLose");
 	}
 
 	public void StartGame()
