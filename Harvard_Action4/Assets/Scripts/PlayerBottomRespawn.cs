e5760aa15253a099c6c70b8e89f32d47484b231a
AlanApogee <alangvhuang@outlook.com>
2022-04-24 22:19:43 +0800

    Cleaned Bottom Respawn

--- Harvard_Action4/Assets/Scripts/PlayerBottomRespawn.cs
+++ Harvard_Action4/Assets/Scripts/PlayerBottomRespawn.cs
@@ -4,22 +4,16 @@ using UnityEngine;
 
 public class PlayerBottomRespawn : MonoBehaviour
 {
-
-    public GameHandler gameHandler;
+    private GameHandler gameHandler;
     public Transform playerPos;
     public Transform pSpawn;
     public int damage = 10;
-    private GameHandler gameHandlerObj;
 
 
     void Start()
     {
         playerPos = GameObject.FindWithTag("Player").GetComponent<Transform>();
         gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
-        if (GameObject.FindWithTag("GameHandler") != null)
-        {
-            gameHandlerObj = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
-        }
     }
 
     void Update()
@@ -30,7 +24,7 @@ public class PlayerBottomRespawn : MonoBehaviour
             {
                 //instantiate a particle effect
                 Debug.Log("I am going back to the start");
-                gameHandlerObj.playerLoseEssence(damage);
+                gameHandler.playerLoseEssence(damage);
                 Vector3 pSpn2 = new Vector3(pSpawn.position.x, pSpawn.position.y, playerPos.position.z);
                 playerPos.position = pSpn2;
             }
