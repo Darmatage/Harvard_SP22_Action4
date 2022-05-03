439e2add948b659f9e0cb0829956f3aa92d682c1
AlanApogee <alangvhuang@outlook.com>
2022-05-04 03:47:31 +0800

    Revert "Edit Ladder Script"
    This reverts commit 45198702bef85587cac4d891926982015b6d8f80.

--- Harvard_Action4/Assets/Scripts/Hazards/LadderMove.cs
+++ Harvard_Action4/Assets/Scripts/Hazards/LadderMove.cs
@@ -9,7 +9,6 @@ public class LadderMove : MonoBehaviour
     private Rigidbody2D playerRB;
     public float upSpeed = 10f;
     private Vector3 vMove;
-    public GameObject marker;
 
     public bool canLadder = false;
 
