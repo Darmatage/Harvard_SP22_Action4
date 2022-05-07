a791193e63c88e69943bc4cb7ac35da2ee2a60d2
pkramadhati <60539575+pkramadhati@users.noreply.github.com>
2022-05-07 02:26:18 -0700

    Level 1 Art Complete - Scroll Not Working

---
+++ Harvard_Action4/Assets/Scripts/Background/backgroundLoopSimple.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class backgroundLoopSimple : MonoBehaviour {
+      private Transform centerBG;
+      public float offset = 33f;       //this value is the width of the image
+
+      void Update(){
+            if (transform.position.x >= centerBG.position.x + offset){
+                  centerBG.position = new Vector2(transform.position.x + offset, centerBG.position.y);
+            }
+            else if (transform.position.x <= centerBG.position.x - offset){
+                  centerBG.position = new Vector2(transform.position.x - offset, centerBG.position.y);
+            }
+      }
+}
