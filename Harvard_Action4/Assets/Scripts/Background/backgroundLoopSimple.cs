b6f07abefd1b51cccabb0bb4c6440fdc57a28a1b
AlanApogee <alangvhuang@outlook.com>
2022-05-09 18:43:49 +0800

    Level 1 Background Update

--- Harvard_Action4/Assets/Scripts/Background/backgroundLoopSimple.cs
+++ Harvard_Action4/Assets/Scripts/Background/backgroundLoopSimple.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class backgroundLoopSimple : MonoBehaviour {
       public Transform centerBG;
-      public float offset = 33f;       //this value is the width of the image
+      private Transform PlayerPos;
+      private Transform CamPos;
+      private float offset = 33f;
+	  public float yMod = 1f;
+	  public float xMod = 1f;
+	  private float chunks = 0f;
+	  
+	  void Awake() {
+			PlayerPos = GameObject.FindWithTag("Player").GetComponent<Transform>();
+			CamPos = transform;
+	  }
 
       void Update(){
-            if (transform.position.x >= centerBG.position.x + offset){
-                  centerBG.position = new Vector2(transform.position.x + offset, centerBG.position.y);
+            if (PlayerPos.position.x >= centerBG.position.x + offset){
+                  chunks = chunks + 1f;
             }
-            else if (transform.position.x <= centerBG.position.x - offset){
-                  centerBG.position = new Vector2(transform.position.x - offset, centerBG.position.y);
+            else if (PlayerPos.position.x <= centerBG.position.x - offset){
+                  chunks = chunks - 1f;
             }
+			centerBG.position = new Vector2(CamPos.position.x/xMod + chunks * offset, CamPos.position.y/yMod);
       }
 }
